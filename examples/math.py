6065c8013d16655b1669cbe87f33854a8d2ea76e
alesisce <aleixcotaina@gmail.com>
2025-08-26 13:19:54 +0200

    modify example

--- examples/math.py
+++ examples/math.py
@@ -24,7 +24,7 @@ def sub(num1, num2=0):
     else:
         print(value)
 
-argflow.new_argument("--append", lambda: setattr(Values, "AppendValue", True))
-argflow.new_argument("--add", add, allow_multiple=True)
-argflow.new_argument("--sub", sub, allow_multiple=True)
+argflow.new_argument("append", lambda: setattr(Values, "AppendValue", True))
+argflow.new_argument("add", add, allow_multiple=True)
+argflow.new_argument("sub", sub, allow_multiple=True)
 argflow.parse()
