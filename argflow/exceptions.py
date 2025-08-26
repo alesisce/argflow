0348bd467e635dd051cea6b3e7f0e88ce7ea406b
alesisce <aleixcotaina@gmail.com>
2025-08-26 13:10:48 +0200

    Removing unused exceptions

--- argflow/exceptions.py
+++ argflow/exceptions.py
@@ -22,30 +22,16 @@ class exceptions:
         def __init__(self, *args):
             super().__init__(*args)
 
+    class InvalidArgumentName(Exception):
+        """
+        Triggered when the argument can't have some characters
+        """
+        def __init__(self, *args):
+            super().__init__(*args)
+
     class NoArgumentFound(Exception):
         """
         Exception only triggered when the argument you specified is not found.
-        """
-        def __init__(self, *args):
-            super().__init__(*args)
-
-    class ToMuchPositionals(Exception):
-        """
-        Only happens when there is to much positionals to handle and functions only can handle a certain amount.
-        """
-        def __init__(self, *args):
-            super().__init__(*args)
-
-    class MissingPositionalArgument(Exception):
-        """
-        Exception triggered when arguments are not optional and are missing.
-        """
-        def __init__(self, *args):
-            super().__init__(*args)
-
-    class ParseFail(Exception):
-        """
-        Exception when the parsing fails. Rarely happens.
         """
         def __init__(self, *args):
             super().__init__(*args)
